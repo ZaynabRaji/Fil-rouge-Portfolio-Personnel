239edcd852495f85025f759a5d2f3049926bf5af
zaynabraji <zaynab.raji@gmail.com>
2024-11-12 00:50:32 +0100

    START JS

--- FilRougePP.js
+++ FilRougePP.js
@@ -1,1 +1,76 @@
+// 1
+let popupRecipes = document.getElementById("popupRecipes");
+function openPopupRecipes(){
+  popupRecipes.classList.add("openpopup1");
+}
+function closePopupRecipes(){
+  popupRecipes.classList.remove("openpopup1");
+}
+// 2
+let popupMTS = document.getElementById("popupMTS");
+function openPopupMTS(){
+  popupMTS.classList.add("openpopup2");
+}
+function closePopupMTS(){
+  popupMTS.classList.remove("openpopup2");
+}
+// 3
+let popupPortfolio = document.getElementById("popupPortfolio");
+function openPopupPortfolio(){
+  popupPortfolio.classList.add("openpopup3");
+}
+function closePopupPortfolio(){
+  popupPortfolio.classList.remove("openpopup3");
+}
+// 4
+let popupNote = document.getElementById("popupNote");
+function openPopupNote(){
+  popupNote.classList.add("openpopup4");
+}
+function closePopupNote(){
+  popupNote.classList.remove("openpopup4");
+}
+// 5
+let popupTodo = document.getElementById("popupTodo");
+function openPopupTodo(){
+  popupTodo.classList.add("openpopup5");
+}
+function closePopupTodo(){
+  popupTodo.classList.remove("openpopup5");
+}
+// 6
+let popupMeal = document.getElementById("popupMeal");
+function openPopupMeal(){
+  popupMeal.classList.add("openpopup6");
+}
+function closePopupMeal(){
+  popupMeal.classList.remove("openpopup6");
+}
+// 7
+let popupWeather = document.getElementById("popupWeather");
+function openPopupWeather(){
+  popupWeather.classList.add("openpopup7");
+}
+function closePopupWeather(){
+  popupWeather.classList.remove("openpopup7");
+}
+
+
+// const ContactForm = document.getElementsById('ContactForm');
+// contactForm.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   emailSend();
+// });
+
+// function emailSend() {
+//   Email.send({
+//     Host: "smtp.elasticemail.com",
+//     Username: "zaynab.raji@gmail.com",
+//     Password: "9700407B194F1FFD1E8609134E474F965896",
+//     To: "zaynab.raji@gmail.com",
+//     From: "zaynab.raji@gmail.com",
+//     Subject: "This is the subject",
+//     Body: "And this is the body",
+//   }).then((message) => alert(message));
+// }
 
